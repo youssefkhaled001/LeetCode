edfa2847b5f02edb6ac71c8afce8b0c24ccdd2d5
Youssef Khaled <youssefkhaled001@outlook.com>
2022-10-19 23:25:12 +0200

    Time: 58 ms (41.52%), Space: 13.8 MB (79.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,18 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        MIN = ord('a')
-        
-        res = []
-        nums = []
-        
-        for i in digits:
-            num = int(i)
-            nums.append(num)
-
         if(digits == ''):
-            return res
+            return []
+        
+        dict = {'2': 'abc', '3':'def','4':'ghi', '5':'jkl', '6':'mno', '7': 'pqrs', '8':'tuv','9':'wxyz'}
+        res = []
 
         def solve(s, *args):
             if(len(args) == 0):
                 res.append(s)
             else:
-                sevenAdder = 0 if args[0] <8 else 1
-                for i in range(4 if args[0] == 7 or args[0] == 9 else 3):
-                    newS = s
-                    char = chr((args[0]-2)*3 + MIN + i + sevenAdder)
-                    newS += char
-                    solve(newS, *args[1:])
+                for i in dict[args[0]]:
+                    solve(s+i, *args[1:])
+               
 
-
-
-
-        solve("", *nums)
+        solve("", *digits)
         return res
